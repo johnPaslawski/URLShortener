f3784b778f83dcf66f65b04632840d33728a90f1
johnPaslawski <jpaslawski93@gmail.com>
2021-10-01 00:35:47 +0200

    Domain Models added, DTO objects accordingly added, DbContext basic structure added

---
+++ URLShortener.Domain/Models/Link.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace URLShortener.Domain.Models
+{
+    public class Link
+    {
+        [Key]
+        public int Id { get; set; }
+        public string GenuineURL { get; set; }
+        public string ShortenedURL { get; set; }
+    }
+}
---
+++ URLShortener.Domain/Models/Reports.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System;
+
+namespace URLShortener.Domain.Models
+{
+    public class Reports
+    {
+        public int Id { get; set; }
+        public DateTime CreationTime  { get; set; }
+        public string Author  { get; set; }
+        public string Description  { get; set; }
+        public string Content  { get; set; }
+    }
+}
